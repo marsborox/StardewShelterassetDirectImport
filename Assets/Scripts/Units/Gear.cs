609b4857bd92215d089bf1061634f3c422597747
marsborox <mierkamaros@gmail.com>
2024-09-21 22:17:10 +0200

    attempt to do object pooling

---
+++ Assets/Scripts/Units/Gear.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Gear : MonoBehaviour
+{
+    public string Armor;
+    public string Helmet;
+
+
+    public string Weapon;
+    public string Firearm;
+    public string Shield;
+    public string Cape;
+    public string Back;
+    public string Mask;
+    public string Horns;
+
+    //chest
+    //head
+    //weapon -1H, 2H, ranged, shield, 
+
+
+
+
+}
