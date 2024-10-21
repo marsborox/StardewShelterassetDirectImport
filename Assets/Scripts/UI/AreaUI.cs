adc368084b4e102d91231db30e174b40b82fe88e
mierka_m <162184787+marsborox@users.noreply.github.com>
2024-10-21 23:23:13 +0200

    button rework, some progress on areaUI

---
+++ Assets/Scripts/UI/AreaUI.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using TMPro;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AreaUI : MonoBehaviour
+{
+
+    [SerializeField] GameObject areas;
+    [SerializeField] Button areaButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayAreaButtons();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        DisplayAreaButtons();
+        Debug.Log("Areas Displayed");
+    }
+    void DisplayAreaButtons()
+    {
+        int childCount= areas.transform.childCount;
+        //foreach Gameobject area in areas create button with method
+        for (int i=0; i<childCount; i++) 
+        {
+            Button spawnedButton = Instantiate(areaButton);
+            GameObject area = areas.gameObject.transform.GetChild(i).gameObject;
+            spawnedButton.gameObject.GetComponentInChildren<TextMeshProUGUI>().text="someName"/*area.name*/;
+            //spawnedButton.destination=area;
+        }
+    }
+    void AreaButtonMethod()
+    { 
+        //set target somethign osmething
+    }
+}
